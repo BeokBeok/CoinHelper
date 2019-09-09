65b0b15975b530d1cf73b96b03a4107580448e25
BeokBeok <kekemusa37@nate.com>
2019-09-09 20:19:38 +0900

    DI 관련 모듈 분리

--- BeokBeok/app/src/main/java/com/architecturestudy/di/NetworkModules.kt
+++ BeokBeok/app/src/main/java/com/architecturestudy/di/NetworkModules.kt
@@ -1,32 +1,13 @@
 package com.architecturestudy.di
 
-import androidx.room.Room
 import com.architecturestudy.BuildConfig
-import com.architecturestudy.data.source.UpbitDataSource
-import com.architecturestudy.data.source.UpbitRepository
-import com.architecturestudy.data.source.local.UpbitDatabase
-import com.architecturestudy.data.source.local.UpbitLocalDataSource
-import com.architecturestudy.data.source.remote.UpbitRemoteDataSource
 import com.architecturestudy.data.source.remote.UpbitRemoteService
-import com.architecturestudy.market.UpbitViewModel
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-import org.koin.android.ext.koin.androidApplication
-import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-
-val viewModelModule = module {
-    viewModel { UpbitViewModel(get()) }
-}
-
-val dataSourceModule = module {
-    single { UpbitRepository(get(), get()) }
-    single<UpbitDataSource.Local> { UpbitLocalDataSource(get()) }
-    single<UpbitDataSource.Remote> { UpbitRemoteDataSource(get()) }
-}
 
 fun getRemoteServiceModule(url: String) = module {
     single {
@@ -54,15 +35,4 @@ fun getRemoteServiceModule(url: String) = module {
             .build()
     }
     single { get<Retrofit>().create(UpbitRemoteService::class.java) }
-}
-
-fun getLocalServiceModule() = module {
-    single {
-        Room.databaseBuilder(
-            androidApplication(),
-            UpbitDatabase::class.java,
-            "UpbitTicker.db"
-        ).build()
-    }
-    single { get<UpbitDatabase>().upbitTickerDao() }
 }
